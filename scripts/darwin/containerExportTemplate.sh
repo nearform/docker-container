ce938fb460e6053be68f2d5de4cb8dd93e4ca206
Peter Elger <elger.peter@gmail.com>
2014-09-23 16:46:56 +0100

    merged local and remote docker funcitonality

--- scripts/darwin/containerExportTemplate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TMPID=$(docker run -d __NAMESPACE__/__TARGETNAME__-__BUILDNUMBER__ /bin/bash)
-docker export $TMPID > __BUILDPATH__/__TARGETNAME__-__BUILDNUMBER__
-
--- scripts/linux/containerExportTemplate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TMPID=$(sudo docker run -d __NAMESPACE__/__TARGETNAME__-__BUILDNUMBER__ /bin/bash)
-sudo docker export $TMPID > __BUILDPATH__/__TARGETNAME__-__BUILDNUMBER__
-
